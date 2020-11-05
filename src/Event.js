2eb5efd7b0e9e5f4c1b6f5a1547b4ada0da25129
Plalangan <p.lalangan@hotmail.com>
2020-11-05 12:51:55 -0800

    Added tests
    added tests

--- src/Event.js
+++ src/Event.js
@@ -1,16 +1,54 @@
-import React, { Component } from 'react';
-
-
-const state = {
-    event: ''
-}
+import React, { Component } from "react";
 
 class Event extends Component {
- render() {
+  state = {
+    showDetails: false,
+  };
+
+  handleShowDetails = () => {
+    if (this.state.showDetails === false) {
+      this.setState({ showDetails: true });
+    } else {
+      this.setState({ showDetails: false });
+    }
+  };
+
+  render() {
     return (
-        <ul className="Event">
-        
-        </ul>
+      <div className="event">
+        <div className="event__Overview">
+        <div className="event__Overview--name"><h1>Event:</h1> {this.props.event.name}</div>
+          {this.props.event.group && this.props.event.group.name && (
+            <div className="event__Overview--groupName">
+             <h1>Group:</h1> {this.props.event.group.name}
+            </div>
+          )}
+          
+          <div className="event__Overview--localTime">
+            <h1>Time:</h1> {this.props.event.local_time}
+          </div>
+          <div className="event__Overview--localDate">
+            <h1>Date:</h1> {this.props.event.local_date}
+          </div>
+          <div className="event__Overview--peopleGoing">
+            <br></br>{this.props.event.yes_rsvp_count} people are going!
+          </div>
+         
+          <button
+            className="details-btn"
+            onClick={() => this.handleShowDetails()}
+          >
+            show details
+          </button>
+        </div>
+        {this.state.showDetails && (
+          <div className="event__Details">
+            <div className="event__Details--description">
+              {this.props.event.description}
+            </div>
+          </div>
+        )}
+      </div>
     );
   }
 }
--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -5,7 +5,46 @@ import Event from '../Event';
 describe("<Event/> Component", () => {
     let EventWrapper;
     beforeAll(() => {
-        EventWrapper = shallow(<Event/>)
+        const event = {
+            created: 1487190400000,
+            duration: 3600000,
+            id: "lmqbqmybckbtb",
+            name: "Creative Protesting",
+            date_in_series_pattern: false,
+            status: "upcoming",
+            time: 1594864800000,
+            local_date: "2020-07-15",
+            local_time: "19:00",
+            rsvp_open_offset: "PT168H",
+            rsvp_close_offset: "PT48H",
+            updated: 1487193351000,
+            utc_offset: -25200000,
+            waitlist_count: 0,
+            yes_rsvp_count: 0,
+            is_online_event: false,
+            group: {
+              created: 1486184133000,
+              name: "#Resist: Burnaby",
+              id: 22223764,
+              join_mode: "open",
+              lat: 49.2599983215332,
+              lon: -122.86000061035156,
+              urlname: "Resist-Burnaby",
+              who: "Members",
+              localized_location: "Burnaby, BC",
+              state: "BC",
+              country: "ca",
+              region: "en_US",
+              timezone: "Canada/Pacific",
+            },
+            link: "https://www.meetup.com/Resist-Burnaby/events/lmqbqmybckbtb/",
+            description:
+              "<p>Let's Meet up and talk about the challenges we face and will be facing as Protesters and find creative way to get our message out there</p> ",
+            visibility: "public",
+            pro_is_email_shared: false,
+            member_pay_fee: false,
+          };
+        EventWrapper = shallow(<Event event={event}/>)
     });
 
     test('render the component', () => {
