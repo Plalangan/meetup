e9318a68d361cb4b06b3d31a9368e8a76c63d24c
Plalangan <p.lalangan@hotmail.com>
2020-11-09 13:42:45 -0800

    added alert component
    added alert component

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
 import { getSuggestions } from './api';
+import  InfoAlert  from './Alert';
 
 class CitySearch extends Component {
 
 state = {
     query: 'Munich',
-    suggestions: [ ]
+    suggestions: [ ],
+    infoText: ''
 }
 
 handleInputChanged = (event) => {
     const value = event.target.value;
-    this.setState({query: value});
-    getSuggestions(value).then(suggestions => 
-        this.setState({suggestions}))
+    this.setState({ query: value});
+    getSuggestions(value).then(suggestions => {
+        this.setState({suggestions});
+
+        if (value && suggestions.length === 0){
+            this.setState({
+                infoText: 'We can not find the city you are looking for. Please try another city',
+            });
+        } else {
+            this.setState({
+                infoText: '',
+            });
+        }
+    });
 }
 
 handleItemClicked = (value, lat, lon) => {
@@ -24,6 +37,7 @@ handleItemClicked = (value, lat, lon) => {
  render() {
     return (
         <div className="CitySearch">
+            <InfoAlert text={this.state.infoText}/>
             <input type="text" className="city" value={this.state.query} onChange={this.handleInputChanged}/>
             <ul className="suggestions">
                 {this.state.suggestions.map(item =>
